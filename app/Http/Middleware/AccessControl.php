1328019b40602f22f33014ae235fc34491faad1d
Nate Denlinger <ndenlinger@midaseducation.com>
2016-07-05 16:55:04 -0400

    allow options calls

--- app/Http/Middleware/AccessControl.php
+++ app/Http/Middleware/AccessControl.php
@@ -16,7 +16,7 @@ class AccessControl
     public function handle($request, Closure $next)
     {
         $response = $next($request);
-        $response->header('Access-Control-Allow-Methods', 'HEAD, GET, POST, PUT, PATCH, DELETE');
+        $response->header('Access-Control-Allow-Methods', 'HEAD, GET, POST, PUT, PATCH, DELETE, OPTIONS');
         $response->header('Access-Control-Allow-Headers', $request->header('Access-Control-Request-Headers'));
         $response->header('Access-Control-Allow-Origin', '*');
         return $response;
