5233a4ce7c1a5d89773f317df24f19b2687d99b5
Nate Denlinger <ndenlinger@midaseducation.com>
2016-07-05 14:33:28 -0400

    changed route for user status to fit with frontend

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -4,7 +4,7 @@ Route::get('/users/oauth','UserController@getLink');
 Route::get('/users/oauth/return',['as'=>'twitter.callback','uses'=>'UserController@returnOauth']);
 
 Route::group(['middleware' => ['cors','oauth']],function(){
-    Route::get('/users/status','UserController@getStatus');
+    Route::get('/users/me','UserController@getStatus');
     Route::get('/tweets','TweetController@getIndex');
     Route::post('/tweets','TweetController@postTweet');
     Route::get('/tweets/{id}',['middleware'=>['tweet'],'uses'=>'TweetController@getTweet']);
