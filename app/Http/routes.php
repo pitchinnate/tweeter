4dd52a24671edf06bf71bf15c5acb8e93437c02f
Nate Denlinger <ndenlinger@midaseducation.com>
2016-07-06 12:37:43 -0400

    fix routing middleware

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -7,6 +7,9 @@ Route::group(['middleware' => ['oauth']],function(){
     Route::get('/users/me','UserController@getStatus');
     Route::get('/tweets','TweetController@getIndex');
     Route::post('/tweets','TweetController@postTweet');
+});
+
+Route::group(['middleware' => ['oauth','tweet']],function(){
     Route::get('/tweets/{id}',['middleware'=>['tweet'],'uses'=>'TweetController@getTweet']);
     Route::put('/tweets/{id}',['middleware'=>['tweet'],'uses'=>'TweetController@putTweet']);
     Route::delete('/tweets/{id}',['middleware'=>['tweet'],'uses'=>'TweetController@deleteTweet']);
